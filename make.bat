96e6c4035cce19206d49db101304638ef64f3d75
Aiwantaozi <michelia.feng@gmail.com>
2019-07-08 16:54:01 +0800

    support windows logging
    Problem:
    Logging feature hasn't supported in windows yet
    
    Solution:
    Add logging windows supported
    
    Issue:
    http://github.com/rancher/rancher/issues/20510

---
+++ make.bat
@@ -0,0 +1,52 @@
+@echo off
+if "%1%"=="trash" (
+    call :trash
+    goto :eof
+)
+if "%1%"=="trash-keep" (
+    call :trash-keep
+    goto :eof
+)
+if "%1%"=="deps" (
+    call :deps
+    goto :eof
+)
+if "%1%"=="update-gemfile" (
+    call :update-gemfile
+    goto :eof
+)
+if "%1%"=="" (
+    set cmd=ci
+) else (
+    set cmd=%1%
+)
+call :.dapper
+.dapper.exe -f Dockerfile-windows.dapper %cmd%
+goto :eof
+
+:.dapper
+if not exist .dapper.exe (
+    bitsadmin /rawreturn /transfer dappwer-download https://releases.rancher.com/dapper/latest/dapper-Windows-x86_64.exe %~dp0\.dapper.exe
+    .dapper.exe -v
+)
+goto :eof
+
+:update-gemfile
+call :.dapper
+.dapper.exe -f Dockerfile-windows.dapper ci
+powershell ./scripts/windows/upgrade-gems.ps1
+goto :eof
+
+:trash
+call :.dapper
+.dapper.exe -f Dockerfile-windows.dapper -m bind trash.exe
+goto :eof
+
+:trash-keep
+call :.dapper
+.dapper.exe -f Dockerfile-windows.dapper -m bind trash.exe -k
+goto :eof
+
+:deps
+call :trash
+goto :eof
---
+++ scripts/windows/package.ps1
@@ -0,0 +1,35 @@
+#Requires -Version 5.0
+$ErrorActionPreference = "Stop"
+
+Invoke-Expression -Command "$PSScriptRoot\get-image.ps1"
+
+$DIR_PATH = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$SRC_PATH = (Resolve-Path "$DIR_PATH\..\..").Path
+cd $SRC_PATH\package\windows
+
+$ARCH = $env:ARCH
+$TAG = $env:TAG
+$IMAGE = $env:IMAGE
+$HOST_RELEASE_ID = $env:HOST_RELEASE_ID
+$RELEASE_ID = $env:RELEASE_ID
+
+if ($RELEASE_ID -eq $HOST_RELEASE_ID) {
+    docker build `
+        --build-arg SERVERCORE_VERSION=$RELEASE_ID `
+        --build-arg ARCH=$ARCH `
+        --build-arg VERSION=$TAG `
+        -t $IMAGE `
+        -f Dockerfile .
+} else {
+    docker build `
+        --isolation hyperv `
+        --build-arg SERVERCORE_VERSION=$RELEASE_ID `
+        --build-arg ARCH=$ARCH `
+        --build-arg VERSION=$TAG `
+        -t $IMAGE `
+        -f Dockerfile .
+}
+
+$DIST_PATH = "$SRC_PATH\dist\"
+$null = New-Item -Type Directory -Path $DIST_PATH -ErrorAction Ignore
+echo $IMAGE > "$DIST_PATH\images"
